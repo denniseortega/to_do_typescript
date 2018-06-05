ea963d38b81556dce3b3b088ead734e7483e6ff0
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-05 15:08:53 -0700

    add initial files

---
+++ app/to-do.js
@@ -0,0 +1,13 @@
+var Task = /** @class */ (function () {
+    function Task(description, priority) {
+        this.description = description;
+        this.priority = priority;
+        this.done = false;
+    }
+    return Task;
+}());
+var tasks = [];
+tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+console.log(tasks);
